4b32311e4f8be58257d6efe91db63311aa3acb94
Emilie Luong <emi.luong@gmail.com>
2023-07-07 20:37:08 +0200

    done without styling

--- app.js
+++ app.js
@@ -1,19 +1,76 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const app = express();
+const bodyParser = require("body-parser");
+// 2. let know your app you will be using it
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // require spotify-web-api-node package here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
 
-const app = express()
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
 
-app.use(expressLayouts)
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.use(express.static(__dirname + '/public'))
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
 
 // setting the spotify-api goes here:
 
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'))
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);
+
+app.get("/", (request, response) => {
+  response.render("home");
+});
+
+app.get("/artist-search", async (request, response) => {
+  try {
+    const body = request.query;
+    const artistsSearch = await spotifyApi.searchArtists(body.artist);
+    console.log("The received data from the API: ", artistsSearch.body);
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    response.render("artist-search-results", {
+      artists: artistsSearch.body.artists.items,
+    });
+  } catch (err) {
+    console.log("The error while searching artists occurred: ", err);
+  }
+});
+
+app.get("/albums/:artistId", async (request, response) => {
+  try {
+    const artistId = request.params.artistId;
+    const artistAlbums = await spotifyApi.getArtistAlbums(artistId);
+    response.render("albums", { albums: artistAlbums.body.items });
+  } catch (err) {
+    console.log("The error while searching artist's albums occurred: ", err);
+  }
+});
+
+app.get("/tracks/:albumId", async (request, response) => {
+  try {
+    const albumId = request.params.albumId;
+    const albumTracks = await spotifyApi.getAlbumTracks(albumId);
+    console.log(albumTracks.body);
+response.render("tracks", {tracks : albumTracks.body.items})
+  } catch (err) {
+    console.log("The error while searching album's tracks occurred: ", err);
+  }
+});
